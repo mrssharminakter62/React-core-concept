3c3521ea15ed962e4e12034e6b3ff3626e6e4120
Mrs Sharmin Akter <zamalzamalhossain@gmail.com>
2021-02-24 10:41:57 +0100

    added file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import { getSuggestedQuery } from '@testing-library/react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const megaFriend = {
+    name:'Sabia',
+    age: 25,
+    mobile:2322425,
+    job: 'facebooking'
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <Counter></Counter>
+     <Users></Users>
+     <Person name = {megaFriend.name}  job= {megaFriend.job} ></Person>
+     <Person name = {megaFriend.name}  job= {megaFriend.job} ></Person>
       </header>
     </div>
   );
 }
 
+function Counter(){
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onMouseMove ={ ()=> setCount(count - 1)}>Decrease</button>
+      <button onClick={ () => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
+
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  })
+  return (
+    <div>
+      <h3>Dynamic Users:{users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li> )
+        }
+      </ul>
+    </div>
+  )
+}
+
+function Person(props){
+ const personStyle ={
+   color: "red",
+   border:'5px solid grey',
+   fontSize:'30px'
+ }
+  return(
+    <div style={personStyle}>
+      <h1>My friend: {props.name}</h1>
+      <h3>Job: {props.job}</h3>
+    </div>
+  )
+}
 export default App;
